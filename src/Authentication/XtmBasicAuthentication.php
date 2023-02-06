f7b4711506ba4d0db25431c89facf153c9acc220
Oliver Davies <oliver@oliverdavies.uk>
2023-02-06 16:23:25 +0000

    feat(basic-auth): return the token from the
    ...response

--- src/Authentication/XtmBasicAuthentication.php
+++ src/Authentication/XtmBasicAuthentication.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace Opdavies\XtmConnect\Authentication;
 
 use Opdavies\XtmConnect\Authentication\AuthenticationMethodInterface as AuthenticationAuthenticationMethodInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class XtmBasicAuthentication implements AuthenticationAuthenticationMethodInterface
 {
+    public function __construct(
+        private HttpClientInterface $httpClient,
+    ) {}
+
     public function getToken(): string
     {
-        return '';
+        $response = $this->httpClient->request('POST', '');
+        $responseData = $response->toArray();
+
+        return $responseData['token'];
     }
 }
--- tests/Authentication/XtmBasicAuthenticationTest.php
+++ tests/Authentication/XtmBasicAuthenticationTest.php
@@ -5,16 +5,23 @@ declare(strict_types=1);
 namespace Tests\Opdavies\XtmConnect\Authentication;
 
 use Opdavies\XtmConnect\Authentication\XtmBasicAuthentication;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 use PHPUnit\Framework\TestCase;
 
 final class XtmBasicAuthenticationTest extends TestCase
 {
     /** @test */
-    public function should_return_a_token(): void
+    public function should_return_a_valid_token(): void
     {
-        $token = (new XtmBasicAuthentication())
-            ->getToken();
+        $expectedResponseData = ['token' => 'valid-token'];
+        $mockResponseJson = json_encode($expectedResponseData, JSON_THROW_ON_ERROR);
+        $mockResponse = new MockResponse($mockResponseJson);
 
-        self::assertIsString($token);
+        $httpClient = new MockHttpClient($mockResponse);
+
+        $xtmBasicAuthentication = new XtmBasicAuthentication($httpClient);
+
+        self::assertSame('valid-token', $xtmBasicAuthentication->getToken());
     }  
 }
