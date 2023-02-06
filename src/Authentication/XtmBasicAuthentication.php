bfa9ad4cfc03428ed7af2c2c9ea69cd70f38811e
Oliver Davies <oliver@oliverdavies.uk>
2023-02-06 16:23:25 +0000

    feat(basic-auth): throw error for no client ID

--- src/Authentication/XtmBasicAuthentication.php
+++ src/Authentication/XtmBasicAuthentication.php
@@ -6,15 +6,27 @@ namespace Opdavies\XtmConnect\Authentication;
 
 use Opdavies\XtmConnect\Authentication\AuthenticationMethodInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Webmozart\Assert\Assert;
 
 final class XtmBasicAuthentication implements AuthenticationMethodInterface
 {
+    private string $clientId = '';
+
     public function __construct(
         private HttpClientInterface $httpClient,
     ) {}
 
+    public function forClientId(string $clientId): self
+    {
+        $this->clientId = $clientId;
+
+        return $this;
+    }
+
     public function getToken(): string
     {
+        Assert::stringNotEmpty($this->clientId);
+
         $response = $this->httpClient->request('POST', '');
         $responseData = $response->toArray();
 
--- tests/Authentication/XtmBasicAuthenticationTest.php
+++ tests/Authentication/XtmBasicAuthenticationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Opdavies\XtmConnect\Authentication;
 
+use InvalidArgumentException;
 use Opdavies\XtmConnect\Authentication\XtmBasicAuthentication;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
@@ -20,8 +21,23 @@ final class XtmBasicAuthenticationTest extends TestCase
 
         $httpClient = new MockHttpClient($mockResponse);
 
-        $xtmBasicAuthentication = new XtmBasicAuthentication($httpClient);
+        $xtmBasicAuthentication = (new XtmBasicAuthentication($httpClient))
+            ->forClientId('client-id');
 
         self::assertSame('valid-token', $xtmBasicAuthentication->getToken());
     }  
+
+    /** @test */
+    public function should_throw_an_exception_if_there_is_no_client_id(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $expectedResponseData = [];
+        $mockResponseJson = json_encode($expectedResponseData, JSON_THROW_ON_ERROR);
+        $mockResponse = new MockResponse($mockResponseJson, ['http_code' => 400]);
+
+        $httpClient = new MockHttpClient($mockResponse);
+
+        (new XtmBasicAuthentication($httpClient))->getToken();
+    }
 }
